6d6c81a096e0997129f64b3f7cb81fd75a49dda6
thanhkowibu <131281853+thanhkowibu@users.noreply.github.com>
2025-04-29 23:45:20 +0700

    initial commit

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex-center h-screen bg-yellow-100">
+      <div className="h1 text-red-400">GG Drive Clone</div>
+    </div>
+  );
+}
